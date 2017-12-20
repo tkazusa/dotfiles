e984f01623fe3fe17a185d73f120eb2daef009e1
Taketoshi Kazusa <taketoshi.kazusa@brainpad.co.jp>
2017-12-20 12:10:53 +0900

    fix

--- install.d/link.sh
+++ install.d/link.sh
@@ -3,10 +3,6 @@
 ln -s $DOTDIR/.tmux.conf $HOME
 ln -s $DOTDIR/.vim $HOME
 
-# tmux plugins
-TMUXDIR=$HOME/.tmux
-mkdir -p $TMUXDIR/plugins
-ln -s $HOME/src/github.com/tmux-plugins/tpm $TMUXDIR/plugins/tpm
 
 # neovim
 ln -s $DOTDIR/.vim/vimrc $DOTDIR/nvim/init.vim
