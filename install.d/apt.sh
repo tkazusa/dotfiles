7a8c3af2c0dc158fa25479128bee67e3ba244671
Taketoshi Kazusa <taketoshi.kazusa@brainpad.co.jp>
2017-12-20 15:42:36 +0900

    fix

--- install.d/apt.sh
+++ install.d/apt.sh
@@ -10,10 +10,10 @@ $APT_CMD gawk git golang vim wget tar libevent-dev libncurses-dev
 #apt-get Update
 #apt-get install -y tmux
 
-# Steps to build and install tmux from source on Ubuntu.
-# Takes < 25 seconds on EC2 env [even on a low-end config instance].
+
 VERSION=2.5
 apt-get -y remove tmux
+apt-get -y install wget tar libevent-dev libncurses-dev
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar xf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
