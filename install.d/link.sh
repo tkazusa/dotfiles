a79378413a3c71150bb01ca4b655ae74427bab09
Kazusa <tkazusa@users.noreply.github.com>
2018-01-10 22:35:32 +0900

    Update link.sh

--- install.d/link.sh
+++ install.d/link.sh
@@ -6,9 +6,14 @@ ln -s $DOTDIR/.vim $HOME
 
 
 # .config
-ln -s $DOTDIR/.config $HOME/.config
+mkdir -p $HOMR/.config/nvim
+ln -s $DOTDIR/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -s $DOTDIR/.config/nvim/colors $HOME/.config/nvim/colors
+ln -s $DOTDIR/.config/fish ln -s $HOME/.config/fish 
 
 # bashrc
 ln -s $DOTDIR/.bashrc $HOME/.bashrc
 ln -s $DOTDIR/.bash_profile $HOME/.bash_profile
 
+
+  
