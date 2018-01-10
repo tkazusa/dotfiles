988ac1292ecd8a209658260a5a70ac8fba5194ae
Kazusa <tkazusa@users.noreply.github.com>
2018-01-10 19:17:53 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,8 @@ set -g -x PATH /usr/local/bin $PATH
 function fish_user_key_bindings
     bind \cr peco_select_history
 end
+
+export TERM=xterm-256color
+alias v 'nvim'
+alias ll 'ls -la'
+alias ps 'ps -aux'
