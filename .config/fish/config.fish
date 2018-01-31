2964927d2c596575d703e87359818b6b1dbced7d
tkazusa <takekazusa@gmail.com>
2018-01-31 13:03:06 +0000

    mod fish right prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,26 @@ function fish_prompt
     echo $pwd $p
 end
 
+
+#Git
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+set __fish_git_prompt_char_dirtystate '✗'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles 'Z'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+
+function fish_right_prompt
+    set last_status $status
+    printf '%s ' (__fish_git_prompt)
+    set_color normal
+end
