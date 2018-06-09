2d97efe791d6713631c323e0c06301e8e1bb17a9
tkazusa <takekazusa@gmail.com>
2018-06-09 22:02:21 +0000

    mod

--- install.d/apt.sh
+++ install.d/apt.sh
@@ -5,24 +5,23 @@ APT_CMD="apt-get install -y"
 $APT_CMD gawk git golang vim wget tar libevent-dev libncurses-dev
 
 # tmux
-#add-apt-repository -y ppa:pi-rho/dev
-#apt-get Update
-#apt-get install -y tmux
+add-apt-repository -y ppa:pi-rho/dev
+apt-get Update
+apt-get install -y tmux
 
-
-VERSION=2.5
-apt-get -y remove tmux
-apt-get -y install wget tar libevent-dev libncurses-dev
-wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
-tar xf tmux-${VERSION}.tar.gz
-rm -f tmux-${VERSION}.tar.gz
-cd tmux-${VERSION}
-./configure
-make
-make install
-cd -
-rm -rf /usr/local/src/tmux-*
-mv tmux-${VERSION} /usr/local/src
+#VERSION=2.5
+#apt-get -y remove tmux
+#apt-get -y install wget tar libevent-dev libncurses-dev
+#wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+#tar xf tmux-${VERSION}.tar.gz
+#rm -f tmux-${VERSION}.tar.gz
+#cd tmux-${VERSION}
+#./configure
+#make
+#make install
+#cd -
+#rm -rf /usr/local/src/tmux-*
+#mv tmux-${VERSION} /usr/local/src
 
 
 # build tools
@@ -64,3 +63,5 @@ wget https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_amd64.tar.
 tar zxvf peco_linux_amd64.tar.gz
 cp peco_linux_amd64/peco /usr/local/bin/
 chmod 777 /usr/local/bin/peco
+rm -rf peco_linux_amd64.tar.gz
+
