36dbd5212f3c352e6dbc5d4d49f8c2f34def4482
tkazusa <takekazusa@gmail.com>
2019-02-27 03:45:00 +0000

    add rsync

--- install.d/apt-get.sh
+++ install.d/apt-get.sh
@@ -5,17 +5,18 @@ apt-get update
 
 #common
 $APT_CMD \
-	git \
-	htop \
-	curl \
-	cmake \
-	less \
-	man \
-	pip3 \
-	tmux \ 
-	unzip \
-	vim \
-	wget \
+	git    \
+	htop   \
+	curl   \
+	cmake  \
+	less   \
+	man    \
+	pip3   \
+  rsync  \
+	tmux   \ 
+	unzip  \
+	vim    \
+	wget   \
 
 
 #Language
