5d6a42fd87a07d1053e0cceadf59753f47fc0caa
tkazusa <takekazusa@gmail.com>
2019-02-27 04:14:28 +0000

    mod setup.sh

--- install.d/install-fish.sh
+++ install.d/install-fish.sh
@@ -2,5 +2,8 @@
 APT_CMD="apt-get install -y"
 DOTDIR=$HOME/dotfiles
 
+# fish
 $APT_CMD fish
+
+# fisher
 curl https://git.io/fisher --create-dirs -sLo DOTDIR/.config/fish/functions/fisher.fish
--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 DOTDIR=$HOME/dotfiles
 
+# --------------------
+# Install packages
+# --------------------
+source $DOTDIR/install.d/apt-get.sh
 source $DOTDIR/install.d/install-fish.sh
+source $DOTDIR/install.d/nvim.sh
+source $DOTDIR/install.d/pip3.sh
+source $DOTDIR/install.d/link.sh
+
+# -------------------
+# Set gitconfigs
+# -------------------
+touch $HOME/.gitconfig
+if ! `grep "\[include\]" $HOME/.gitconfig > /dev/null`; then
+  cat << EOF >> $HOME/.gitconfig
+[include]
+	path = $DOTDIR/.gitconfig.local
+EOF
+fi
