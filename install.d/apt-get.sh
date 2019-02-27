35c8c9143b564c159c9357976ffd35098fe1cfc1
tkazusa <takekazusa@gmail.com>
2019-02-27 12:59:49 +0000

    mod apt-get.sh

--- install.d/apt-get.sh
+++ install.d/apt-get.sh
@@ -3,24 +3,11 @@ APT_CMD="apt-get install -y"
 
 apt-get update
 
-#common
-$APT_CMD \
-	git    \
-	htop   \
-	curl   \
-	cmake  \
-	less   \
-	man    \
-	python3-pip \
-  	rsync  \
-	tmux   \ 
-	unzip  \
-	vim    \
-	wget   \
+# common
+$APT_CMD git htop curl cmake man python3-pip rsync unzip vim wget
 
+# tmux
+$APT_CMD tmux
 
 #Language
-$APT_CMD \
-	language-pack-ja-base \
-	language-pack-ja \
-	ibus-mozc
+$APT_CMD language-pack-ja-base language-pack-ja ibus-mozc
