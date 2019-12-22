2256146de0e5401b644145bd62bd78cecffec4ac
Kazusa <tkazusa@users.noreply.github.com>
2019-12-23 00:30:46 +0900

    Update pip3.sh

--- install.d/pip3.sh
+++ install.d/pip3.sh
@@ -1,7 +1,7 @@
-pip3 install autopep8 flake8 isort neovim python-language-server  jupyter  
+pip3 install autopep8 flake8 isort python-language-server jupyter  
 
 pip3 install sklearn matplotlib
 
 # For AWS developmenmt
 pip3 install boto3
-pip3 install awscli --upgrade --user
+pip3 install --upgrade --user awscli 
